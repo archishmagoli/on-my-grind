5b72752235b21e905794882051a3e64271ee3bf7
archishmagoli <agoli6@gatech.edu>
2024-05-07 16:24:48 -0400

    crewmate style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Form from './components/Form';
+import Coffee from './assets/coffee-icon.png'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
       <div className='header'>
         <div className='intro'>
           <h1>On My Grind</h1>
-          <img id='intro-image' src="../assets/coffee-icon.png"/>
+          <img id='intro-image' src={Coffee}/>
         </div>
         <p>So you think you can barista? Let's put that to the test...</p>
         <br></br>
